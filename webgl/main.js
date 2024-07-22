d2e3bea43f98524da0fab4ded902440dfb731496
Antoine Cornaz <antoine.cornaz01@gmail.com>
2024-07-22 10:03:15 +0200

    mvp and cube working

--- webgl/main.js
+++ webgl/main.js
@@ -6,7 +6,7 @@ import {icg_mesh_load_obj} from "../icg/icg_mesh.js"
 import {deg_to_rad, mat4_to_string, vec_to_string, mat4_matmul_many} from "../icg/icg_math.js"
 import { mesh_preprocess } from "../icg/normal_computation.js"
 import {mat3, mat4, vec3, vec4} from "../lib/gl-matrix/index.js";
-import {lookAt} from "../lib/gl-matrix/mat4.js";
+import {lookAt, perspective} from "../lib/gl-matrix/mat4.js";
 
 
 
@@ -88,13 +88,19 @@ async function main() {
               }`,
 
         vert: `
-              attribute vec2 position;
+              attribute vec3 position;
+              uniform mat4 u_mat_mvp;
               void main() {
-                gl_Position = vec4(position, 0, 1);
+                vec4 pos = u_mat_mvp * vec4(position, 1.0);
+                gl_Position = pos;
               }`,
 
         attributes: {
             position: [[0.5, -0.5], [-0.5, -0.5], [0, 0.5]]
+        },
+
+        uniforms: {
+            u_mat_mvp: regl.prop("u_mat_mvp"),
         },
 
         count: 3
@@ -105,33 +111,41 @@ async function main() {
     // Set clear color to sky, fully opaque
     const skyColor = colors.sky.concat(1.0);
 
-
-
-
     regl.frame((frame) => {
 
-        const x = Math.cos(frame.time/100)
-        const y = Math.sin(frame.time/100)
+        const x = Math.cos(frame.time)
+        const y = Math.sin(frame.time)
 
-        const eye = vec3.fromValues(0.8, 1.5, 1.2)
+        const eye = vec3.fromValues(x*4, y*3, 3)
         const center = vec3.fromValues(0, 0, 0)
         const up = vec3.fromValues(0, 0.0,0.8)
 
 
 
-        const look = mat4.lookAt(
+        const view = mat4.lookAt(
             mat4.create(),
             eye,
             center,
             up
         )
 
-        let a = mat4.fromScaling(mat4.create(), vec3.fromValues(0.3,0.5,0.5))
-        mat4.rotateX(a, a, 1)
-        mat4.rotateY(a, a, frame.time)
+        const projection = perspective(
+            mat4.create(),
+            Math.PI/2.0,
+            1,
+            0.1,
+            100
+        )
+
+        const mvp = mat4.mul(mat4.create(), projection, view);
         const cube_meshes_props = []
         cube_meshes_props.push({
-            u_mat_mvp: a
+            u_mat_mvp: mvp
+        })
+
+        const triangle_meshes_props = []
+        triangle_meshes_props.push({
+            u_mat_mvp: mvp
         })
 
         regl.clear({
@@ -139,7 +153,7 @@ async function main() {
         });
 
 
-        drawTriangle()
+        drawTriangle(triangle_meshes_props)
         drawCube(cube_meshes_props)
     });
 }
