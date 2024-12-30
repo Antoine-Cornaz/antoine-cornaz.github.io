3e618954b54751a5a41c7f6c9e791f4e92f22838
Antoine Cornaz <antoine.cornaz01@gmail.com>
2024-12-30 21:19:02 +0100

    add touch with finger 6

--- webgl/control.js
+++ webgl/control.js
@@ -30,8 +30,8 @@ export function addListener(window, canvas, player){
         const touch = event.touches[0];
     
         // Calculate normalized coordinates
-        const normalizedX = 2*touch.clientX / canvas.width - 1.0;
-        const normalizedY = -2*touch.clientY / canvas.height + 1.0;
+        const normalizedX = touch.clientX / canvas.width;
+        const normalizedY = -touch.clientY / canvas.height;
     
         // Move the player based on the touch position
         player.move(vec2.fromValues(normalizedX, normalizedY));
